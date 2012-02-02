ed61c114b9b91d68a2667876c6dc56dfa19bd4c5
dmbst32 <dmbst32@gmail.com>
2012-02-02 07:10:48 +0000

    Added hack for ohava computers with no serial number

--- client_scripts/inventory.sh
+++ client_scripts/inventory.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-cp /opt/crb/user_inventory.sh /opt/user_inventory_real.sh
+cp /opt/user_inventory.sh /opt/user_inventory_real.sh
 
 manufacturer=$(/usr/sbin/dmidecode -s chassis-manufacturer)
+
 # hack because the first serial number is blank on s10-3
 serial=$(/usr/sbin/dmidecode | /bin/grep -E 'Serial Number:[[:space:]]*[^[:space:]]+' | /bin/sed 's/.*: \(.*\)/\1/;q')
+
+# Hack for Ohava computers that don't fill out serial number
+dumb_serial='To be filled by O.E.M.'
+if [ "$serial" = "$dumb_serial" ]
+then
+    serial=$(ifconfig | grep HWaddr | while read line; do echo $line | awk '{ print $1 " " $5 }'; done | sed 1q)
+fi
+
 model=$(/usr/sbin/dmidecode -s system-product-name)
 dCount=$(/sbin/fdisk -l | /bin/grep '^Disk /dev/' | wc -l)
 
